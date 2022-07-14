f031053a6519b7321de06ca82cdc4c98b74eab70
IrynaVlasenko <106557356+IrynaVlasenko@users.noreply.github.com>
2022-07-14 21:47:36 +0300

    Update the time

--- js.js
+++ js.js
@@ -100,6 +100,30 @@
 // let button2 = document.querySelector("#current-button");
 // button2.addEventListener("click", getCurrentLocation);
 // search("Texas");
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -112,6 +136,8 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#today");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "fb4ad69a2bb4c6370849b9a18c3de8e4";
